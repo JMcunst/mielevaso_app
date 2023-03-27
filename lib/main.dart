3ac470da7121f72258ef4e140e2c9fa0804e627d
JMcunst <xnslqjtmghaf@gmail.com>
2023-03-27 20:38:48 +0900

    update : main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,59 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:mielevaso_app/screens/LoginPage.dart';
+import 'package:mielevaso_app/screens/UserPage.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:logger/logger.dart';
+import 'package:mielevaso_app/screens/MainPage.dart';
+import 'dart:developer';
 
-void main() {
+var logger = Logger(
+  printer: PrettyPrinter(),
+);
+
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+ThemeData appTheme = ThemeData(
+    primaryColor: Colors.purple,
+    /* Colors.tealAccent,*/
+    secondaryHeaderColor: Colors.blue /* Colors.teal*/
+  // fontFamily:
+);
+
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class _MyAppState extends State<MyApp>{
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    return GetMaterialApp(
+      title: '로그인 앱',
+      theme: appTheme,
+      home: MainPage()
     );
   }
+
+  @override
+  void initState() {
+    super.initState();
+
+  }
+
+  @override
+  dispose() async {
+    super.dispose();
+
+  }
+
 }
---
+++ lib/screens/MainPage.dart
@@ -0,0 +1,63 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mielevaso_app/screens/GuildPage.dart';
+
+import 'LoginPage.dart';
+
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context){
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+
+  @override
+  void initState(){
+    super.initState();
+
+    // _permission();
+    _logout();
+    _auth();
+
+  }
+
+  @override
+  void dispose(){
+    super.dispose();
+  }
+
+  // 제거해도 되는 부분이나, 추후 권한 설정과 관련된 포스팅 예정
+  // _permission() async{
+  //   Map<Permission, PermissionStatus> statuses = await [
+  //     Permission.storage,
+  //   ].request();
+  //   //logger.i(statuses[Permission.storage]);
+  // }
+
+  _auth(){
+    // 사용자 인증정보 확인. 딜레이를 두어 확인
+    Future.delayed(const Duration(milliseconds: 100),() {
+      if(FirebaseAuth.instance.currentUser == null){
+        Get.off(() => const LoginPage());
+      } else {
+        Get.off(() => const GuildPage());
+      }
+    });
+  }
+
+  _logout() async{
+    await FirebaseAuth.instance.signOut();
+  }
+}
