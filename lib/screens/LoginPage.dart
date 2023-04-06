b39bbe366c02a88b805f54da0a048a49404bb15a
JMcunst <xnslqjtmghaf@gmail.com>
2023-04-06 16:25:18 +0900

    feat : gesture detector in login page

--- lib/screens/LoginPage.dart
+++ lib/screens/LoginPage.dart
@@ -1,16 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-import 'GuildPage.dart';
 import 'HomePage.dart';
 import 'RegisterPage.dart';
-import 'MainPage.dart';
 import '../main.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'dart:developer';
-
-import 'UserPage.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -21,12 +17,13 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final TextEditingController _emailController = TextEditingController(); //입력되는 값을 제어
+  final TextEditingController _emailController =
+      TextEditingController(); //입력되는 값을 제어
   final TextEditingController _passwordController = TextEditingController();
 
   String _imageFile = 'assets/images/main.png';
 
-  Widget _userIdWidget(){
+  Widget _userIdWidget() {
     return TextFormField(
       controller: _emailController,
       keyboardType: TextInputType.emailAddress,
@@ -34,8 +31,9 @@ class _LoginPageState extends State<LoginPage> {
         border: OutlineInputBorder(),
         labelText: '이메일',
       ),
-      validator: (String? value){
-        if (value!.isEmpty) {// == null or isEmpty
+      validator: (String? value) {
+        if (value!.isEmpty) {
+          // == null or isEmpty
           return '이메일을 입력해주세요.';
         }
         return null;
@@ -43,7 +41,7 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  Widget _passwordWidget(){
+  Widget _passwordWidget() {
     return TextFormField(
       controller: _passwordController,
       obscureText: true,
@@ -52,8 +50,9 @@ class _LoginPageState extends State<LoginPage> {
         border: OutlineInputBorder(),
         labelText: '비밀번호',
       ),
-      validator: (String? value){
-        if (value!.isEmpty) {// == null or isEmpty
+      validator: (String? value) {
+        if (value!.isEmpty) {
+          // == null or isEmpty
           return '비밀번호를 입력해주세요.';
         }
         return null;
@@ -69,34 +68,44 @@ class _LoginPageState extends State<LoginPage> {
         title: const Text("로그인"),
         centerTitle: true,
       ),
-      body: Form(
-        key: _formKey,
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              Image(width: 400.0, height: 250.0, image: AssetImage(_imageFile)),
-              const SizedBox(height: 20.0),
-              _userIdWidget(),
-              const SizedBox(height: 20.0),
-              _passwordWidget(),
-              Container(
-                height: 70,
-                width: double.infinity,
-                padding: const EdgeInsets.only(top: 8.0), // 8단위 배수가 보기 좋음
-                child: ElevatedButton(
-                    onPressed: () => _login(),
-                    child: const Text("로그인")
+      body: GestureDetector(
+        // 변경된 부분: GestureDetector 추가
+        onTap: () {
+          // 키보드 숨기기
+          FocusScopeNode currentFocus = FocusScope.of(context);
+          if (!currentFocus.hasPrimaryFocus &&
+              currentFocus.focusedChild != null) {
+            currentFocus.focusedChild?.unfocus();
+          }
+        },
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                Image(
+                    width: 400.0, height: 250.0, image: AssetImage(_imageFile)),
+                const SizedBox(height: 20.0),
+                _userIdWidget(),
+                const SizedBox(height: 20.0),
+                _passwordWidget(),
+                Container(
+                  height: 70,
+                  width: double.infinity,
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: ElevatedButton(
+                      onPressed: () => _login(), child: const Text("로그인")),
                 ),
-              ),
-              const SizedBox(height: 20.0),
-              GestureDetector(
-                child: const Text('회원 가입'),
-                onTap: (){
-                  Get.to(() => const RegisterPage());
-                },
-              ),
-            ],
+                const SizedBox(height: 20.0),
+                GestureDetector(
+                  child: const Text('회원 가입'),
+                  onTap: () {
+                    Get.to(() => const RegisterPage());
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -108,6 +117,7 @@ class _LoginPageState extends State<LoginPage> {
     //해당 클래스가 호출되었을떄
     super.initState();
   }
+
   @override
   void dispose() {
     // 해당 클래스가 사라질떄
@@ -117,50 +127,41 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   _login() async {
-    //키보드 숨기기
     if (_formKey.currentState!.validate()) {
-      FocusScope.of(context).requestFocus(FocusNode());
-
-      // Firebase 사용자 인증, 사용자 등록
-      try {
-        await FirebaseAuth.instance.signInWithEmailAndPassword(
-          email: _emailController.text,
-          password: _passwordController.text,
-        );
-
-        User user = FirebaseAuth.instance.currentUser!;
-        DocumentSnapshot userDetails =
-        await FirebaseFirestore.instance.collection('user').doc(user.uid).get();
-        bool isDetail = userDetails.exists && ((userDetails.data() as Map<String, dynamic>)['is_detail'] ?? false);
-        log('TTTTTT: $userDetails.data()');
-        log('VVVVVV: $isDetail');
-        if (isDetail) {
-          // 사용자의 디테일 정보가 있는 경우
-          Get.offAll(() => HomePage(initialPageIndex: 1));
-        } else {
-          // 사용자의 디테일 정보가 없는 경우
-          Get.offAll(() => HomePage(initialPageIndex: 0));
-        }
-      } on FirebaseAuthException catch (e) {
-        logger.e(e);
-        String message = '';
-
-        if (e.code == 'user-not-found') {
-          message = '사용자가 존재하지 않습니다.';
-        } else if (e.code == 'wrong-password') {
-          message = '비밀번호를 확인하세요';
-        } else if (e.code == 'invalid-email') {
-          message = '이메일을 확인하세요.';
-        }
-
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text(message),
-            backgroundColor: Colors.deepOrange,
-          ),
-        );
+      // 키보드 숨기기
+      FocusScopeNode currentFocus = FocusScope.of(context);
+      if (!currentFocus.hasPrimaryFocus && currentFocus.focusedChild != null) {
+        currentFocus.focusedChild?.unfocus();
       }
 
+      try {
+        UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: _emailController.text.trim(),
+          password: _passwordController.text.trim(),
+        );
+        if (userCredential.user != null) {
+          // 로그인 성공
+          Get.offAll(() => const HomePage());
+        }
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'user-not-found') {
+          // 이메일이나 비밀번호가 잘못됨
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('이메일이나 비밀번호가 잘못되었습니다.'),
+            ),
+          );
+        } else if (e.code == 'wrong-password') {
+          // 이메일이나 비밀번호가 잘못됨
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('이메일이나 비밀번호가 잘못되었습니다.'),
+            ),
+          );
+        }
+      } catch (e) {
+        log(e.toString());
+      }
     }
   }
 
