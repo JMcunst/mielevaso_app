e79a993a695ab01a0fb1d74b6c4ceb52a3355ad4
JMcunst <xnslqjtmghaf@gmail.com>
2023-04-08 15:09:42 +0900

    feat : floating button

--- lib/screens/EquipmentPage.dart
+++ lib/screens/EquipmentPage.dart
@@ -12,17 +12,148 @@ class EquipmentPage extends StatefulWidget {
 class _EquipmentPageState extends State<EquipmentPage>
     with SingleTickerProviderStateMixin {
   late TabController _tabController;
+  late User user;
+  late List<Map<String, dynamic>> allList;
+  late List<Map<String, dynamic>> swordList;
+  late List<Map<String, dynamic>> helmetList;
+  late List<Map<String, dynamic>> armorList;
+  late List<Map<String, dynamic>> necklaceList;
+  late List<Map<String, dynamic>> ringList;
+  late List<Map<String, dynamic>> shoesList;
+
+  final _categoryFormField = ['sword', 'helmet', 'armor', 'necklace', 'ring', 'shoes'];
+  String _categoryValue ='';
+  final _convertedField = ['없음', 0, 1, 2, 3];
+  String _convertedFormValue ='';
+  final _gradeFormField = ['전설', '영웅', '희귀', '일반'];
+  String _gradeValue ='';
+  final _isBalcksmithFormField = ['가능', '불가능', '제련됨'];
+  String _isBalcksmithValue ='';
+  int _level = 0;
+  final _statTypeFormField = [{'code': 'HPP', 'title': '생명력'},{'code': 'HPS', 'title': '생명력(%)'},
+    {'code': 'DFP', 'title': '방어력'}, {'code': 'DFS', 'title': '방어력(%)'},
+    {'code': 'ATP', 'title': '공격력'}, {'code': 'ATS', 'title': '공격력(%)'},
+    {'code': 'CHC', 'title': '치명확률'}, {'code': 'CHD', 'title': '치명피해'},
+    {'code': 'EFV', 'title': '효과적중'}, {'code': 'EFR', 'title': '효과저항'},
+    {'code': 'SPD', 'title': '속도'}];
+  int _mainStatPoint = 0;
+  int _subStatPointOne = 0;
+  int _subStatPointTwo = 0;
+  int _subStatPointThree = 0;
+  int _subStatPointFour = 0;
+  String _name = '';
+  final _reinforced = 15;
+  final _setTypeFormField = [{'code': 'ATK', 'title': '공격'},{'code': 'DEF', 'title': '방어'},
+    {'code': 'HLP', 'title': '생명'}, {'code': 'SPD', 'title': '속도'},
+    {'code': 'CTR', 'title': '치확'}, {'code': 'DES', 'title': '파멸'},
+    {'code': 'HTR', 'title': '적중'}, {'code': 'RES', 'title': '저항'},
+    {'code': 'LFS', 'title': '흡혈'}, {'code': 'CNT', 'title': '반격'},
+    {'code': 'DUA', 'title': '협공'}, {'code': 'IMM', 'title': '면역'},
+    {'code': 'RAG', 'title': '분노'}, {'code': 'PEN', 'title': '관통'},
+    {'code': 'REV', 'title': '복수'}, {'code': 'INJ', 'title': '상처'},
+    {'code': 'TOR', 'title': '격류'}, {'code': 'PRO', 'title': '수호'}];
+  String _setType = '';
+
 
   @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 7, vsync: this);
+    user = FirebaseAuth.instance.currentUser!;
+    allList = [];
+    swordList = [];
+    helmetList = [];
+    armorList = [];
+    necklaceList = [];
+    ringList = [];
+    shoesList = [];
+    _loadEquipments();
   }
 
   @override
   void dispose() {
     _tabController.dispose();
     super.dispose();
+  }
+
+  Future<void> _loadEquipments() async {
+    final equipmentsSnapshot = await FirebaseFirestore.instance
+        .collection('equipments')
+        .doc(user.uid)
+        .get();
+    final testData = [equipmentsSnapshot.data()];
+    print('TTTTT:$testData');
+    if (equipmentsSnapshot.exists) {
+      setState(() {
+        swordList = List<Map<String, dynamic>>.from(
+            equipmentsSnapshot.data()!['sword'] ?? []);
+        helmetList = List<Map<String, dynamic>>.from(
+            equipmentsSnapshot.data()!['helmet'] ?? []);
+        armorList = List<Map<String, dynamic>>.from(
+            equipmentsSnapshot.data()!['armor'] ?? []);
+        necklaceList = List<Map<String, dynamic>>.from(
+            equipmentsSnapshot.data()!['necklace'] ?? []);
+        ringList = List<Map<String, dynamic>>.from(
+            equipmentsSnapshot.data()!['ring'] ?? []);
+        shoesList = List<Map<String, dynamic>>.from(
+            equipmentsSnapshot.data()!['shoes'] ?? []);
+        allList = [
+          ...swordList,
+          ...helmetList,
+          ...armorList,
+          ...necklaceList,
+          ...ringList,
+          ...shoesList,
+        ];
+      });
+    }
+  }
+
+  Widget _buildTabView(List<Map<String, dynamic>> equipments) {
+    return ListView.builder(
+      itemCount: equipments.length,
+      itemBuilder: (context, index) {
+        final equipment = equipments[index];
+        return Card(
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Image.asset(
+                  'assets/images/main.png',
+                  width: 64,
+                  height: 64,
+                ),
+                SizedBox(width: 16.0),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(equipment['name']),
+                    SizedBox(height: 4.0),
+                    Text(equipment['set'].replaceAll('SET_', '')),
+                    SizedBox(width: 8.0),
+                    Text(equipment['grade']),
+                  ],
+                ),
+                SizedBox(width: 16.0),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text('장비 점수'),
+                    SizedBox(width: 8.0),
+                    Text(
+                      equipment['score'].toStringAsFixed(1),
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+
   }
 
   @override
@@ -77,128 +208,140 @@ class _EquipmentPageState extends State<EquipmentPage>
               ),
             ),
           ],
-          onTap: (index) {
-            switch (index) {
-              case 0:
-                _tabController.animateTo(0);
-                break;
-              case 1:
-                _tabController.animateTo(1);
-                _buildEquipmentList('sword');
-                break;
-              case 2:
-                _tabController.animateTo(2);
-                _buildEquipmentList('helmet');
-                break;
-              case 3:
-                _tabController.animateTo(3);
-                _buildEquipmentList('armor');
-                break;
-              case 4:
-                _tabController.animateTo(4);
-                _buildEquipmentList('necklace');
-                break;
-              case 5:
-                _tabController.animateTo(5);
-                _buildEquipmentList('ring');
-                break;
-              case 6:
-                _tabController.animateTo(6);
-                _buildEquipmentList('shoes');
-                break;
-              default:
-                break;
-            }
-          },
         ),
       ),
       body: TabBarView(
         controller: _tabController,
         children: [
-          _buildEquipmentList('all'),
-          _buildEquipmentList('sword'),
-          _buildEquipmentList('helmet'),
-          _buildEquipmentList('armor'),
-          _buildEquipmentList('necklace'),
-          _buildEquipmentList('ring'),
-          _buildEquipmentList('shoes'),
+          _buildTabView(allList),
+          _buildTabView(swordList),
+          _buildTabView(helmetList),
+          _buildTabView(armorList),
+          _buildTabView(necklaceList),
+          _buildTabView(ringList),
+          _buildTabView(shoesList),
         ],
       ),
-    );
-  }
-
-  Widget _buildEquipmentList(String category) {
-    final user = FirebaseAuth.instance.currentUser;
-    if (user == null) {
-      // 사용자가 로그인하지 않았을 경우 로그인 페이지로 이동하거나 알림을 표시할 수 있습니다.
-      return Center(
-        child: Text('로그인이 필요합니다.'),
-      );
-    }
-    return FutureBuilder<QuerySnapshot>(
-      future: FirebaseFirestore.instance
-          .collection('equipments')
-          .doc(user.uid)
-          .collection('equipments')
-          .where('category', isEqualTo: category == 'all' ? '' : category)
-          .get(),
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator());
-        } else {
-          final QuerySnapshot querySnapshot = snapshot.data!;
-          final List<DocumentSnapshot> documents = querySnapshot.docs;
-          return documents.isEmpty
-              ? const Center(
-            child: Text('장비가 없습니다.'),
-          )
-              : ListView.builder(
-            itemCount: documents.length,
-            itemBuilder: (BuildContext context, int index) {
-              final Map<String, dynamic> data =
-              documents[index].data()! as Map<String, dynamic>;
-              return ListTile(
-                title: Text(data['name']),
-                subtitle: Text(data['category']),
-                trailing: IconButton(
-                  icon: const Icon(Icons.delete),
-                  onPressed: () {
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: const Text('삭제 확인'),
-                          content: const Text('정말 삭제하시겠습니까?'),
-                          actions: <Widget>[
-                            TextButton(
-                              child: const Text('취소'),
-                              onPressed: () {
-                                Navigator.pop(context);
-                              },
-                            ),
-                            TextButton(
-                              child: const Text('삭제'),
-                              onPressed: () async {
-                                await FirebaseFirestore.instance
-                                    .collection('equipments')
-                                    .doc(user.uid)
-                                    .collection('equipments')
-                                    .doc(documents[index].id)
-                                    .delete();
-                                Navigator.pop(context);
-                              },
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  },
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // showDialog 띄우기
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text('장비 정보 추가'),
+                content: Form(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      DropdownButtonFormField(
+                        decoration:
+                        const InputDecoration(labelText: 'Category'),
+                        value: _categoryValue,
+                        items: _categoryFormField.map((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        onChanged: (String? value) {
+                          setState(() {
+                            _categoryValue = value ?? '';
+                          });
+                        },
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                            labelText: '이름'),
+                        initialValue: _name,
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter item name';
+                          }
+                          return null;
+                        },
+                        onSaved: (String? value) {
+                          _name = value ?? '';
+                        },
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                            labelText: '강화'),
+                        initialValue: _reinforced.toString(),
+                        enabled: false,
+                      ),
+                      DropdownButtonFormField(
+                        decoration:
+                        const InputDecoration(labelText: 'Grade'),
+                        value: _gradeValue,
+                        items: _gradeFormField.map((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        onChanged: (String? value) {
+                          setState(() {
+                            _gradeValue = value ?? '';
+                          });
+                        },
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                            labelText: '레벨'),
+                        initialValue: _level.toString(),
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a level';
+                          }
+                          return null;
+                        },
+                        onSaved: (String? value) {
+                          if (value != null) {
+                            _level = int.parse(value);
+                          }
+                        },
+                      ),
+                      DropdownButtonFormField(
+                        decoration: const InputDecoration(labelText: 'Set'),
+                        value: _setType,
+                        items: _setTypeFormField.map((Map<String, String> map) {
+                          return DropdownMenuItem<String>(
+                            value: map['code'],
+                            child: Text(map['title']!),
+                          );
+                        }).toList(),
+                        onChanged: (String? value) {
+                          setState(() {
+                            _setType = value ?? '';
+                          });
+                        },
+                      )
+                    ],
+                  ),
                 ),
+                actions: <Widget>[
+                  TextButton(
+                    child: const Text('취소'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  TextButton(
+                    child: const Text('저장'),
+                    onPressed: () {
+                      // 사용자가 입력한 데이터 출력
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
               );
             },
           );
-        }
-      },
+        },
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 
