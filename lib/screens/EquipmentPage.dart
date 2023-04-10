09e59eeff465f501bfc1aea356273c905c31047e
JMcunst <xnslqjtmghaf@gmail.com>
2023-04-10 21:19:10 +0900

    update : floating button fields

--- lib/screens/EquipmentPage.dart
+++ lib/screens/EquipmentPage.dart
@@ -22,11 +22,11 @@ class _EquipmentPageState extends State<EquipmentPage>
   late List<Map<String, dynamic>> shoesList;
 
   final _categoryFormField = ['sword', 'helmet', 'armor', 'necklace', 'ring', 'shoes'];
-  String _categoryValue ='';
-  final _convertedField = ['없음', 0, 1, 2, 3];
-  String _convertedFormValue ='';
+  String _categoryValue ='sword';
+  final _convertedField = ['없음', '1번째', '2번째', '3번째', '4번째'];
+  String _convertedFormValue ='없음';
   final _gradeFormField = ['전설', '영웅', '희귀', '일반'];
-  String _gradeValue ='';
+  String _gradeValue ='전설';
   final _isBalcksmithFormField = ['가능', '불가능', '제련됨'];
   String _isBalcksmithValue ='';
   int _level = 0;
@@ -37,22 +37,39 @@ class _EquipmentPageState extends State<EquipmentPage>
     {'code': 'EFV', 'title': '효과적중'}, {'code': 'EFR', 'title': '효과저항'},
     {'code': 'SPD', 'title': '속도'}];
   int _mainStatPoint = 0;
+  String _mainStatValue = '생명력';
   int _subStatPointOne = 0;
+  String _subStatValueOne = '생명력';
   int _subStatPointTwo = 0;
+  String _subStatValueTwo = '생명력';
   int _subStatPointThree = 0;
+  String _subStatValueThree = '생명력';
   int _subStatPointFour = 0;
+  String _subStatValueFour = '생명력';
   String _name = '';
   final _reinforced = 15;
-  final _setTypeFormField = [{'code': 'ATK', 'title': '공격'},{'code': 'DEF', 'title': '방어'},
-    {'code': 'HLP', 'title': '생명'}, {'code': 'SPD', 'title': '속도'},
-    {'code': 'CTR', 'title': '치확'}, {'code': 'DES', 'title': '파멸'},
-    {'code': 'HTR', 'title': '적중'}, {'code': 'RES', 'title': '저항'},
-    {'code': 'LFS', 'title': '흡혈'}, {'code': 'CNT', 'title': '반격'},
-    {'code': 'DUA', 'title': '협공'}, {'code': 'IMM', 'title': '면역'},
-    {'code': 'RAG', 'title': '분노'}, {'code': 'PEN', 'title': '관통'},
-    {'code': 'REV', 'title': '복수'}, {'code': 'INJ', 'title': '상처'},
-    {'code': 'TOR', 'title': '격류'}, {'code': 'PRO', 'title': '수호'}];
-  String _setType = '';
+  // final _setTypeFormField = ['공격', '방어', '생명', '속도'];
+  final _setTypeFormField = [
+    {'code': 'ATK', 'title': '공격'},
+    {'code': 'DEF', 'title': '방어'},
+    {'code': 'HLP', 'title': '생명'},
+    {'code': 'SPD', 'title': '속도'},
+    {'code': 'CTR', 'title': '치확'},
+    {'code': 'DES', 'title': '파멸'},
+    {'code': 'HTR', 'title': '적중'},
+    {'code': 'RES', 'title': '저항'},
+    {'code': 'LFS', 'title': '흡혈'},
+    {'code': 'CNT', 'title': '반격'},
+    {'code': 'DUA', 'title': '협공'},
+    {'code': 'IMM', 'title': '면역'},
+    {'code': 'RAG', 'title': '분노'},
+    {'code': 'PEN', 'title': '관통'},
+    {'code': 'REV', 'title': '복수'},
+    {'code': 'INJ', 'title': '상처'},
+    {'code': 'TOR', 'title': '격류'},
+    {'code': 'PRO', 'title': '수호'}
+  ];
+  String _setType = '공격';
 
 
   @override
@@ -271,8 +288,7 @@ class _EquipmentPageState extends State<EquipmentPage>
                         enabled: false,
                       ),
                       DropdownButtonFormField(
-                        decoration:
-                        const InputDecoration(labelText: 'Grade'),
+                        decoration: const InputDecoration(labelText: 'Grade'),
                         value: _gradeValue,
                         items: _gradeFormField.map((String value) {
                           return DropdownMenuItem<String>(
@@ -303,20 +319,200 @@ class _EquipmentPageState extends State<EquipmentPage>
                         },
                       ),
                       DropdownButtonFormField(
-                        decoration: const InputDecoration(labelText: 'Set'),
-                        value: _setType,
-                        items: _setTypeFormField.map((Map<String, String> map) {
+                        decoration: const InputDecoration(labelText: '제련 여부'),
+                        value: _isBalcksmithValue,
+                        items: _isBalcksmithFormField.map((String value) {
                           return DropdownMenuItem<String>(
-                            value: map['code'],
-                            child: Text(map['title']!),
+                            value: value,
+                            child: Text(value),
                           );
                         }).toList(),
                         onChanged: (String? value) {
                           setState(() {
-                            _setType = value ?? '';
+                            _isBalcksmithValue = value ?? '불가능';
                           });
                         },
-                      )
+                      ),
+                      DropdownButtonFormField(
+                        decoration: const InputDecoration(labelText: 'Set'),
+                        value: _setType,
+                        items: _setTypeFormField.map((item) {
+                          return DropdownMenuItem(
+                            value: item['code'],
+                            child: Text(item['title']!),
+                          );
+                        }).toList(),
+                        onChanged: (String? value) {
+                          setState(() {
+                            _setType = value ?? '공격';
+                          });
+                        },
+                      ),
+                      DropdownButtonFormField(
+                        decoration: const InputDecoration(labelText: '메인옵'),
+                        value: _mainStatValue,
+                        items: _statTypeFormField.map((item) {
+                          return DropdownMenuItem(
+                            value: item['code'],
+                            child: Text(item['title']!),
+                          );
+                        }).toList(),
+                        onChanged: (String? value) {
+                          setState(() {
+                            _mainStatValue = value ?? '생명력';
+                          });
+                        },
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(labelText: '메인옵 포인트'),
+                        initialValue: _mainStatPoint.toString(),
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a main point';
+                          }
+                          return null;
+                        },
+                        onSaved: (String? value) {
+                          if (value != null) {
+                            _mainStatPoint = int.parse(value);
+                          }
+                        },
+                      ),
+                      DropdownButtonFormField(
+                        decoration: const InputDecoration(labelText: '부옵1'),
+                        value: _subStatValueOne,
+                        items: _statTypeFormField.map((item) {
+                          return DropdownMenuItem(
+                            value: item['code'],
+                            child: Text(item['title']!),
+                          );
+                        }).toList(),
+                        onChanged: (String? value) {
+                          setState(() {
+                            _subStatValueOne = value ?? '생명력';
+                          });
+                        },
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(labelText: '부옵1 포인트'),
+                        initialValue: _subStatPointOne.toString(),
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a sub option 1 point';
+                          }
+                          return null;
+                        },
+                        onSaved: (String? value) {
+                          if (value != null) {
+                            _subStatPointOne = int.parse(value);
+                          }
+                        },
+                      ),
+                      DropdownButtonFormField(
+                        decoration: const InputDecoration(labelText: '부옵2'),
+                        value: _subStatValueTwo,
+                        items: _statTypeFormField.map((item) {
+                          return DropdownMenuItem(
+                            value: item['code'],
+                            child: Text(item['title']!),
+                          );
+                        }).toList(),
+                        onChanged: (String? value) {
+                          setState(() {
+                            _subStatValueTwo = value ?? '생명력';
+                          });
+                        },
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(labelText: '부옵2 포인트'),
+                        initialValue: _subStatPointTwo.toString(),
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a sub option 2 point';
+                          }
+                          return null;
+                        },
+                        onSaved: (String? value) {
+                          if (value != null) {
+                            _subStatPointTwo = int.parse(value);
+                          }
+                        },
+                      ),
+                      DropdownButtonFormField(
+                        decoration: const InputDecoration(labelText: '부옵3'),
+                        value: _subStatValueThree,
+                        items: _statTypeFormField.map((item) {
+                          return DropdownMenuItem(
+                            value: item['code'],
+                            child: Text(item['title']!),
+                          );
+                        }).toList(),
+                        onChanged: (String? value) {
+                          setState(() {
+                            _subStatValueThree = value ?? '생명력';
+                          });
+                        },
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(labelText: '부옵3 포인트'),
+                        initialValue: _subStatPointThree.toString(),
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a sub option 3 point';
+                          }
+                          return null;
+                        },
+                        onSaved: (String? value) {
+                          if (value != null) {
+                            _subStatPointThree = int.parse(value);
+                          }
+                        },
+                      ),
+                      DropdownButtonFormField(
+                        decoration: const InputDecoration(labelText: '부옵4'),
+                        value: _subStatValueFour,
+                        items: _statTypeFormField.map((item) {
+                          return DropdownMenuItem(
+                            value: item['code'],
+                            child: Text(item['title']!),
+                          );
+                        }).toList(),
+                        onChanged: (String? value) {
+                          setState(() {
+                            _subStatValueFour = value ?? '생명력';
+                          });
+                        },
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(labelText: '부옵4 포인트'),
+                        initialValue: _subStatPointFour.toString(),
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a sub option 4 point';
+                          }
+                          return null;
+                        },
+                        onSaved: (String? value) {
+                          if (value != null) {
+                            _subStatPointFour = int.parse(value);
+                          }
+                        },
+                      ),
+                      DropdownButtonFormField(
+                        decoration: const InputDecoration(labelText: '변환 여부'),
+                        value: _convertedFormValue,
+                        items: _convertedField.map((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        onChanged: (String? value) {
+                          setState(() {
+                            _convertedFormValue = value ?? '없음';
+                          });
+                        },
+                      ),
                     ],
                   ),
                 ),
