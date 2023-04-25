5be1550baaa50626931a7c942366f7e9c7d5b2c9
JMcunst <xnslqjtmghaf@gmail.com>
2023-04-25 22:04:34 +0900

    feat : custom colors

---
+++ lib/utils/custom_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const dividerLine = Color(0xFFA9907E);
+  static const cardColor = Color(0xFFABC4AA);
+  static const textColorBlack = Color(0xFF675D50);
+  static const firstGradientColor = Color(0xFFF3DEBA);
+  static const secondGradientColor = Color(0xFFA9907E);
+}
