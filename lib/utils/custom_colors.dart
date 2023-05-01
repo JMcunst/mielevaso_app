9d348ddcc77361cb151826424ffe62acd90c841c
JMcunst <xnslqjtmghaf@gmail.com>
2023-05-01 21:08:28 +0900

    feat : update custom colors

--- lib/utils/custom_colors.dart
+++ lib/utils/custom_colors.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
 
+Color primary = const Color(0xFFA9907E);
+
 class CustomColors {
   static const dividerLine = Color(0xFFA9907E);
   static const cardColor = Color(0xFFABC4AA);
   static const textColorBlack = Color(0xFF675D50);
   static const firstGradientColor = Color(0xFFF3DEBA);
   static const secondGradientColor = Color(0xFFA9907E);
+}
+
+class Styles{
+  static Color primaryColor = primary;
+  static Color textColor = const Color(0xFF3b3b3b);
+  static Color bgColor = const Color(0xFFeeedf2);
+  static Color orangeColor = const Color(0xFF526799);
+  static TextStyle textStyle = TextStyle(fontSize: 16, color: textColor, fontWeight: FontWeight.w500);
+  static TextStyle headLineStyle1 = TextStyle(fontSize: 26, color: textColor, fontWeight: FontWeight.bold);
+  static TextStyle headLineStyle2 = TextStyle(fontSize: 21, color: textColor, fontWeight: FontWeight.bold);
+  static TextStyle headLineStyle3 = TextStyle(fontSize: 17, color: textColor, fontWeight: FontWeight.w500);
+  static TextStyle headLineStyle4 = TextStyle(fontSize: 14, color: textColor, fontWeight: FontWeight.w500);
 }
