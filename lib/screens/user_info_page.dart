9e7fdfbf4f908d3dbffc1c10c503cfb754a05c60
JMcunst <xnslqjtmghaf@gmail.com>
2023-05-01 21:09:09 +0900

    remove : unnecessary resources

--- lib/screens/user_info_page.dart
+++ lib/screens/user_info_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mielevaso_app/screens/user_pvp_page.dart';
 import 'package:mielevaso_app/widgets/user_info_widget.dart';
 
 class UserInfoPage extends StatefulWidget {
@@ -85,102 +86,9 @@ class _UserInfoPageState extends State<UserInfoPage> {
               ),
             ),
             SizedBox(
-              height: 20,
+              height: 10,
             ),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 64),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text('장비 개수'),
-                      Text('111/500')
-                    ],
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text('아레나'),
-                      Image(image: AssetImage("assets/images/main.png")),
-                    ],
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text('실레나'),
-                      Image(image: AssetImage("assets/images/main.png")),
-                    ],
-                  )
-                ],
-              ),
-            ),
-            // GridView.count(
-            //   crossAxisCount: 3,
-            //   childAspectRatio: 1, // 가로 세로 비율을 1:1로 설정
-            //   mainAxisSpacing: 0, // 수직 방향 간격을 0으로 설정
-            //   crossAxisSpacing: 0,
-            //   shrinkWrap: true,
-            //   children: [
-            //     Container(
-            //       decoration: BoxDecoration(
-            //         border: Border.all(color: Colors.black, width: 1),
-            //         color: Colors.blue,
-            //       ),
-            //       child: Text('장비 개수'),
-            //     ),
-            //     Container(
-            //       decoration: BoxDecoration(
-            //         border: Border.all(color: Colors.black, width: 1),
-            //         color: Colors.red,
-            //       ),
-            //       child: Text('아레나'),
-            //     ),
-            //     Container(
-            //       decoration: BoxDecoration(
-            //         border: Border.all(color: Colors.black, width: 1),
-            //         color: Colors.green,
-            //       ),
-            //       child: Text('실레나'),
-            //     ),
-            //     Container(
-            //       decoration: BoxDecoration(
-            //         border: Border.all(color: Colors.black, width: 1),
-            //         color: Colors.yellow,
-            //       ),
-            //       child: Text('111/500'),
-            //     ),
-            //     Container(
-            //       width: 30,
-            //       height: 30,
-            //       decoration: BoxDecoration(
-            //         border: Border.all(color: Colors.black, width: 1),
-            //         color: Colors.orange,
-            //       ),
-            //       child: Image(
-            //         image: AssetImage("assets/images/main.png"),
-            //         fit: BoxFit.cover,
-            //       ),
-            //     ),
-            //     Container(
-            //       width: 30,
-            //       height: 30,
-            //       decoration: BoxDecoration(
-            //         border: Border.all(color: Colors.black, width: 1),
-            //         color: Colors.purple,
-            //       ),
-            //       child: Image(
-            //         image: AssetImage("assets/images/main.png"),
-            //         fit: BoxFit.cover,
-            //       ),
-            //     ),
-            //   ].map((child) => Center(child: child)).toList(),
-            // ),
+            UserPvpPage(),
             UserInfoWidget()
           ],
         ),
